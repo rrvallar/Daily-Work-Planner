10962a982c6e0563b831583f13009725f2befc00
rval95 <rrvallar@gmail.com>
2022-06-04 15:24:44 -0400

    added current date and time

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  //CURRENT DATE AND TIME
+  let a = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  $("#current-date").text(a);
+});
+
+// var now = moment();
+// $("#current-date").
